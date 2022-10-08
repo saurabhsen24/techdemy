e5b06030ff4ec7913d2c1e95ee0120b03cc0ba71
Saurabh Sen <saurabhsensaurabh@gmail.com>
2022-10-08 14:07:06 +0530

    Intitial project setup

---
+++ backend/src/main/java/com/techdemy/security/JwtHelper.java
@@ -0,0 +1,62 @@
+package com.techdemy.security;
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.JWTCreator;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.techdemy.utils.Constants;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationToken;
+import org.springframework.stereotype.Component;
+
+import java.security.interfaces.RSAPrivateKey;
+import java.security.interfaces.RSAPublicKey;
+import java.time.Instant;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.Map;
+
+@Slf4j
+@Component
+public class JwtHelper {
+
+    private final RSAPrivateKey privateKey;
+    private final RSAPublicKey publicKey;
+
+    @Value("${jwtExpirationTime}")
+    private Integer jwtExpirationAmount;
+
+    public JwtHelper(RSAPrivateKey privateKey, RSAPublicKey publicKey) {
+        this.privateKey = privateKey;
+        this.publicKey = publicKey;
+    }
+
+    public String createJwtForClaims(String subject, Map<String,String> claims) {
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTimeInMillis(Instant.now().toEpochMilli());
+        calendar.add(Calendar.MINUTE, jwtExpirationAmount);
+
+        JWTCreator.Builder jwtBuilder = JWT.create().withSubject(subject);
+        claims.forEach(jwtBuilder::withClaim);
+
+        return jwtBuilder
+                .withIssuer("techdemy")
+                .withNotBefore(new Date())
+                .withExpiresAt(calendar.getTime())
+                .sign(Algorithm.RSA256(publicKey,privateKey));
+    }
+
+    public static String getCurrentLoggedInUsername() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        JwtAuthenticationToken token = (JwtAuthenticationToken) authentication;
+        Map<String,Object> claims = token.getTokenAttributes();
+        return claims.get(Constants.CLAIMS_USERNAME).toString();
+    }
+
+    public String getTokenExpirationTime() {
+        return jwtExpirationAmount + Constants.MONTH;
+    }
+
+}
