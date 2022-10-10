95a34df18c1d45b7c5834c16d52c500aee955f55
Saurabh Sen <saurabhsensaurabh@gmail.com>
2022-10-11 00:24:19 +0530

    Formats review repo

--- backend/src/main/java/com/techdemy/repository/ReviewRepository.java
+++ backend/src/main/java/com/techdemy/repository/ReviewRepository.java
@@ -14,13 +14,17 @@ import java.util.Optional;
 
 @Repository
 public interface ReviewRepository extends JpaRepository<Review,Long> {
+
     List<Review> findByCourseId(Long courseId);
+
     @Query(value = "SELECT AVG(rating) FROM review where course_id=:courseId", nativeQuery = true)
     double findAvgRatingByCourseId(@Param("courseId") Long courseId);
+
     @Query(value = "SELECT r.review_id, u.user_name as userName, r.review_comment as reviewComment, " +
             "r.rating as rating FROM review r INNER JOIN user u ON " +
             "r.user_id = u.user_id WHERE r.review_id=:reviewId", nativeQuery = true)
     Optional<ReviewResponseDto> getReview(@Param("reviewId") Long reviewId );
+
 
     @Modifying
     @Transactional
@@ -28,6 +32,7 @@ public interface ReviewRepository extends JpaRepository<Review,Long> {
             "WHERE review_id=:reviewId", nativeQuery = true)
     void updateReview(@Param("reviewComment") String reviewComment, @Param("rating") Integer rating,
                       @Param("reviewId") Long reviewId);
+
     @Query(value = "SELECT r.review_id, u.user_name as userName, r.review_comment as reviewComment, r.rating as rating FROM " +
             "review r INNER JOIN user u ON r.user_id = u.user_id WHERE r.course_id=:courseId", nativeQuery = true)
     List<ReviewResponseDto> getAllReviews(@Param("courseId") Long courseId );
